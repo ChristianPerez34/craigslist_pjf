c1bac23fceea058b16ca082f007f048097fd7121
Christian Pérez Villanueva <perez.villanueva.christian34@gmail.com>
2020-07-01 12:02:34 -0400

    Scrapes gigs that have been posted in the past 3 days

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import csv
 import random
 from datetime import datetime
 
@@ -26,31 +27,32 @@ class CraigslistPJF:
     def select_rand_cities(self, num_cities):
         for i in range(100):
             random.shuffle(self.cities)
-        return self.cities[:num_cities]
+        self.cities = self.cities[:num_cities]
 
     def scrape_gigs(self):
         for link in self.cities:
             gig_page = requests.get(f"{link}search/ggg?")
             gig_soup = BeautifulSoup(gig_page.content, 'html.parser')
             gig_posts = gig_soup.find_all("p", class_="result-info")
+
             for post in gig_posts:
                 date_str = post.find("time").attrs["datetime"]
                 date = datetime.strptime(date_str, "%Y-%m-%d %H:%M").date()
+
                 if (self.today - date).days < 4:
                     gig = post.find(class_="result-title")
                     gig_link = gig.attrs["href"]
-                    gig_link_page = requests.get(gig_link)
-                    gig_link_soup = BeautifulSoup(gig_link_page.content, 'html.parser')
-                    gig_description = gig_link_soup.find("section", id="postingbody")
-                    self.gigs.append((gig_link, gig.text, gig_description))
-            print(post)
-
-            # TODO: Only get gigs that have been posted in the last 3 days. 
-            print(gig_soup)
-        pass
+                    self.gigs.append({'link': gig_link, 'title': gig.text})
+                else:
+                    break
 
     def output_to_excel(self):
-        pass
+        with open('craigslist_gigs.csv', mode='w', encoding='utf-8') as csv_file:
+            fieldnames = ['link', 'title']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(self.gigs)
 
     def scrape_jobs(self):
         pass
@@ -58,6 +60,6 @@ class CraigslistPJF:
 
 pjf = CraigslistPJF(url="https://www.craigslist.org/about/sites#US")
 pjf.get_us_cities_links()
-links = pjf.select_rand_cities(num_cities=5)
+pjf.select_rand_cities(num_cities=5)
 pjf.scrape_gigs()
-print(links)
+pjf.output_to_excel()
